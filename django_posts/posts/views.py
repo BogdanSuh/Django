739f9f40e514ca6bc45b5b8e2ca4330d048eb1cf
BogdanSuh <sbv.220613@gamil.com>
2025-10-16 10:04:58 +0700

    55

--- django_posts/posts/views.py
+++ django_posts/posts/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    data = {"n":0}
+    data = {"a":9}
+    data = {"b":5}
     return render(request,"index.html",context=data)
